c5a6b4e13dac1e1a9b2d69987d9c35e6eb3a5bc4
darwish74 <mohammeddarwish66609@gmail.com>
2025-01-28 20:43:20 +0200

    update admin dashboard

---
+++ InstaStay/Areas/Admin/Controllers/ManageHotels.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Models.IRepositories;
+using Models.Models;
+
+namespace InstaStay.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class ManageHotels : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+        public ManageHotels(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+        public IActionResult ShowAllHotels()
+        {
+            var Hotels = unitOfWork.hotelRepository.Get(includeprops:e=>e.Include(e=>e.Rooms).Include(e=>e.HotelManager));
+            return View(Hotels);
+        }
+    }
+}
