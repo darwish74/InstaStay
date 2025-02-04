a16ca2b9928c116517866ef50a9d41832f4a7fe2
mohamednasser2001 <mmm.zzz155mody@gmail.com>
2025-02-04 17:14:33 +0200

    coupon table

---
+++ Models/Models/Coupon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Models
+{
+    public class Coupon
+    {
+        public int CouponId { get; set; }
+        public string Code { get; set; }
+        public string DiscountType { get; set; } 
+        public decimal DiscountValue { get; set; }
+        public DateTime ValidFrom { get; set; }
+        public DateTime ValidTo { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
